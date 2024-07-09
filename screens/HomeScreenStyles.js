8f7cfd159443d92149cac559f163953bc9107cd8
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-07-09 19:38:06 +0200

    added styles files for homepage and huntpage

---
+++ screens/HomeScreenStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    //hela conteinern 
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  //titeln 
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'grey',
+    padding: 10,
+    borderRadius: 5,
+
+  },
+});
+
+export default styles;
--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,14 +1,18 @@
+
+import styles from './HomeScreenStyles'; 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>Welcome to GeoCaching App!</Text>
-      <Button
-        title="Start a Hunt"
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to GeoCaching App!</Text>
+      <Pressable
+        style={styles.button}
         onPress={() => navigation.navigate('Hunt')}
-      />
+      >
+        <Text style={styles.buttonText}>Start a Hunt</Text>
+      </Pressable>
     </View>
   );
 }
