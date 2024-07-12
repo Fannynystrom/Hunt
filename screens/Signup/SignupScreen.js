85b56a796df36b44c53281581c294a5c9fbd9c30
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-07-12 14:39:06 +0200

    re-editing to login screen when creating a user

--- screens/Signup/SignupScreen.js
+++ screens/Signup/SignupScreen.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
 import { auth, db } from '../../firebaseConfig';
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
+  const navigation = useNavigation(); 
 
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
+      console.log('User created:', user);
 
-      // Spara användarnamnet i Firestore
-      await setDoc(doc(collection(db, 'users'), user.uid), {
-        username: username,
+      // sparar användarnamn i Firestore
+      await setDoc(doc(db, 'users', user.uid), {
         email: email,
+        username: username,
       });
 
-      console.log('User created and username saved:', user.uid);
+      // navigerar till inloggningssidan
+      navigation.navigate('Login');
     } catch (error) {
       console.error('Error signing up:', error);
+      alert(error.message);
     }
   };
 
