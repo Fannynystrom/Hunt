c72f064b2e72a854ff699757b017ca4356a4699f
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-08-12 10:09:11 +0200

    moved styling from profileScreen to ProfileScreenStyles.js

--- screens/Profile/ProfileScreen.js
+++ screens/Profile/ProfileScreen.js
@@ -6,7 +6,7 @@ import { doc, setDoc } from 'firebase/firestore';
 import { db, storage } from '../../firebaseConfig';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { auth } from '../../firebaseConfig'; 
-
+import styles from '../Profile/ProfileScreenStyles';
 const ProfileScreen = ({ navigation }) => {
   const { user, username, imageUri: initialImageUri } = useUser();
   const [imageUri, setImageUri] = useState(initialImageUri);
@@ -109,133 +109,6 @@ const ProfileScreen = ({ navigation }) => {
 };
 
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
-  },
 
-  logoutButton: {
-    position: 'absolute',
-    top: 10,
-    left: 15,
-    zIndex: 1,
-  },
-  logoutButtonText: {
-    fontSize: 29,
-    fontWeight: 'bold',
-    color: '#007BFF',
-  },
-
-
-    // Container för profilbild och redigeringsikon
-  imageContainer: {
-    position: 'relative',
-    marginBottom: 20,
-
-  },
-    // profilbilden
-  profileImage: {
-    width: 250,
-    height: 250,
-    borderRadius: 125,
-    borderWidth: 4, 
-    borderColor: 'black', 
-  },
-    //  platshållarbilden (visas om ingen bild finns)
-  placeholderImage: {
-    width: 250,
-    height: 250,
-    borderRadius: 50,
-    backgroundColor: '#ccc',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 2, 
-    borderColor: '#007BFF', 
-  },
-    // container för redigeringsikon
-  editIcon: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    backgroundColor: '#007BFF',
-    borderRadius: 40,
-    width: 70,
-    height: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 2, 
-    borderColor: '#fff', 
-  },
-    // pennan i redigeringsikon
-  editIconText: {
-    color: '#fff',
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  username: {
-    fontSize: 34,
-    fontWeight: 'bold',
-  },
-
-  //rubrikerna active hunts och planned hunts
-  sectionHeader: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-    marginTop: 40,
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    textAlign: 'center',
-    marginHorizontal: 5,
-  },
-
-//container för createhunt knappen
-  createHuntButton: {
-    backgroundColor: '#007BFF',
-    borderColor: '#000',
-    borderWidth: 1,
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingRight: 30,
-    paddingLeft:30,
-    borderRadius: 5,
-    marginTop: 60,
-    marginLeft: 20,
-    alignSelf: 'flex-start',
-
-  },
-  //texten i knappen createhunt
-  createHuntButtonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-
-  //container för "medalj"
-  medalsContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: '#F8F8F8', 
-    width: '100%',
-    marginTop: 60,
-  },
-  medalsTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#007BFF', 
-  },
-
-});
 
 export default ProfileScreen;
--- screens/Profile/ProfileScreenStyles.js
+++ screens/Profile/ProfileScreenStyles.js
@@ -2,25 +2,131 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
     backgroundColor: '#fff',
   },
-  avatar: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 20,
+
+  logoutButton: {
+    position: 'absolute',
+    top: 10,
+    left: 15,
+    zIndex: 1,
   },
-  name: {
-    fontSize: 20,
+  logoutButtonText: {
+    fontSize: 29,
+    fontWeight: 'bold',
+    color: '#007BFF',
+  },
+
+
+    // Container för profilbild och redigeringsikon
+  imageContainer: {
+    position: 'relative',
+    marginBottom: 20,
+
+  },
+    // profilbilden
+  profileImage: {
+    width: 250,
+    height: 250,
+    borderRadius: 125,
+    borderWidth: 4, 
+    borderColor: 'black', 
+  },
+    //  platshållarbilden (visas om ingen bild finns)
+  placeholderImage: {
+    width: 250,
+    height: 250,
+    borderRadius: 50,
+    backgroundColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2, 
+    borderColor: '#007BFF', 
+  },
+    // container för redigeringsikon
+  editIcon: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#007BFF',
+    borderRadius: 40,
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2, 
+    borderColor: '#fff', 
+  },
+    // pennan i redigeringsikon
+  editIconText: {
+    color: '#fff',
+    fontSize: 28,
     fontWeight: 'bold',
   },
-  email: {
-    fontSize: 16,
-    color: 'gray',
+  username: {
+    fontSize: 34,
+    fontWeight: 'bold',
   },
+
+  //rubrikerna active hunts och planned hunts
+  sectionHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    marginTop: 40,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    textAlign: 'center',
+    marginHorizontal: 5,
+  },
+
+//container för createhunt knappen
+  createHuntButton: {
+    backgroundColor: '#007BFF',
+    borderColor: '#000',
+    borderWidth: 1,
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingRight: 30,
+    paddingLeft:30,
+    borderRadius: 5,
+    marginTop: 60,
+    marginLeft: 20,
+    alignSelf: 'flex-start',
+
+  },
+  //texten i knappen createhunt
+  createHuntButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  //container för "medalj"
+  medalsContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#F8F8F8', 
+    width: '100%',
+    marginTop: 60,
+  },
+  medalsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#007BFF', 
+  },
+
 });
 
 export default styles;
