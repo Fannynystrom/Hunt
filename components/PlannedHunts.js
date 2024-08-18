68ff3fc40d91dc578fce88000878c2bcfcff0285
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-08-18 22:48:20 +0200

    created styling file for PlannedHunt

--- components/PlannedHunts.js
+++ components/PlannedHunts.js
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { db, auth } from '../../firebaseConfig';
 import { collection, query, where, getDocs } from 'firebase/firestore';
+
+const PlannedHuntsScreen = () => {
+    const [hunts, setHunts] = useState([]);
+
+}
+    export default PlannedHuntsScreen;
