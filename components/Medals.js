4edd94bcc16d1c2133fc9a6433407dbbad321dc5
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-08-25 11:25:41 +0200

    now medal is created automatically after completed hunt

--- components/Medals.js
+++ components/Medals.js
@@ -1,35 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, FlatList, Text, Image } from 'react-native';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 import { useUser } from '../context/UserContext';
-import styles from '../screens/Profile/ProfileScreenStyles'; 
+import { useFocusEffect } from '@react-navigation/native';
+import styles from '../screens/Profile/ProfileScreenStyles';
 
 const Medals = () => {
   const { user } = useUser();
   const [medals, setMedals] = useState([]);
 
-  useEffect(() => {
-    const fetchMedals = async () => {
-        try {
-            const medalsRef = collection(db, 'medals');
-            const q = query(medalsRef, where('userId', '==', user.uid));
-            const querySnapshot = await getDocs(q);
-
-            const medals = querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-                        setMedals(medals);
-        } catch (error) {
-            console.error('Error fetching medals:', error);
-        }
-    };
-
+  const fetchMedals = useCallback(async () => {
     if (user) {
-        fetchMedals();
+      try {
+        const medalsRef = collection(db, 'medals');
+        const q = query(medalsRef, where('userId', '==', user.uid));
+        const querySnapshot = await getDocs(q);
+        const fetchedMedals = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setMedals(fetchedMedals);
+      } catch (error) {
+        console.error('Error fetching medals:', error);
+      }
     }
-}, [user]);
+  }, [user]);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchMedals();
+    }, [fetchMedals]) 
+  );
 
   const renderMedal = ({ item }) => (
     <View style={styles.medalItem}>
@@ -42,7 +44,7 @@ const Medals = () => {
     <FlatList
       data={medals}
       renderItem={renderMedal}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       ListEmptyComponent={<Text style={styles.noMedalsText}>You have no medals yet.</Text>}
     />
   );
